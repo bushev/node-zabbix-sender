e63dc0038799f8b3ecd1ddee01d0fcd0d337d472
Yury Bushev <bushevuv@gmail.com>
2022-04-14 09:54:34 -0400

    Add proxy support.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var Net      = require('net'),
-    hostname = require("os").hostname();
+var Net = require('net'),
+    hostname = require("os").hostname(),
+    SocksClient = require('socks').SocksClient;
 
-var ZabbixSender = module.exports = function(opts) {
+var ZabbixSender = module.exports = function (opts) {
     opts = (typeof opts !== 'undefined') ? opts : {};
 
     this.host = opts.host || 'localhost';
@@ -10,12 +11,13 @@ var ZabbixSender = module.exports = function(opts) {
     this.with_ns = opts.with_ns || false;
     this.with_timestamps = this.with_ns || opts.with_timestamps || false;
     this.items_host = opts.items_host || hostname;
+    this.proxy = opts.proxy;
 
     // prepare items array
     this.clearItems();
 }
 
-ZabbixSender.prototype.addItem = function(host, key, value) {
+ZabbixSender.prototype.addItem = function (host, key, value) {
     if (arguments.length < 3) {
         if (arguments.length < 2) {
             throw new Error('addItem requires 2 or 3 arguments');
@@ -23,13 +25,13 @@ ZabbixSender.prototype.addItem = function(host, key, value) {
 
         // if just 2 args provided
         value = key;
-        key   = host;
-        host  = this.items_host;
+        key = host;
+        host = this.items_host;
     }
 
     var length = this.items.push({
-        host:  host,
-        key:   key,
+        host: host,
+        key: key,
         value: value
     });
 
@@ -45,53 +47,36 @@ ZabbixSender.prototype.addItem = function(host, key, value) {
     return this;
 }
 
-ZabbixSender.prototype.clearItems = function() {
+ZabbixSender.prototype.clearItems = function () {
     this.items = [];
     return this;
 }
 
-ZabbixSender.prototype.countItems = function() {
+ZabbixSender.prototype.countItems = function () {
     return this.items.length;
 }
 
-ZabbixSender.prototype.send = function(callback) {
+ZabbixSender.prototype.send = function (callback) {
     // make sure callback is a function
-    callback = (typeof callback === 'function') ? callback : function() {};
+    callback = (typeof callback === 'function') ? callback : function () {
+    };
 
-    var self     = this,
-        error    = false,
-        items    = this.items,
-        data     = prepareData(items, this.with_timestamps, this.with_ns),
-        client   = new Net.Socket(),
-        response = new Buffer(0);
+    var self = this,
+        error = false,
+        items = this.items,
+        data = prepareData(items, this.with_timestamps, this.with_ns),
+        client,
+        response = Buffer.alloc(0);
 
-    // uncoment when debugging
-    //console.log(data.slice(13).toString());
-
-    // reset items array
-    this.clearItems();
-
-    // set socket timeout
-    client.setTimeout(this.timeout);
-
-    client.connect(this.port, this.host, function() {
-        client.write(data);
-    });
-
-    client.on('data', function(data) {
+    function onData(data) {
         response = Buffer.concat([response, data]);
-    });
+    }
 
-    client.on('timeout', function() {
-        error = new Error("socket timed out after " + self.timeout / 1000 + " seconds");
-        client.destroy();
-    });
-
-    client.on('error', function(err) {
+    function onError(err) {
         error = err;
-    });
+    }
 
-    client.on('close', function() {
+    function onClose() {
         // bail out on any error
         if (error) {
             // in case of error, put the items back
@@ -108,7 +93,51 @@ ZabbixSender.prototype.send = function(callback) {
 
         // all clear, return the result
         callback(null, JSON.parse(response.slice(13)), items);
-    });
+    }
+
+    // uncoment when debugging
+    //console.log(data.slice(13).toString());
+
+    // reset items array
+    this.clearItems();
+
+    if (this.proxy) {
+        SocksClient.createConnection({
+            proxy: this.proxy,
+            command: 'connect',
+            destination: {
+                host: this.host,
+                port: this.port
+            },
+            timeout: this.timeout
+        }).then((info) => {
+            const client = info.socket;
+            client.on('data', onData);
+            client.on('error', onError);
+            client.on('close', onClose);
+            client.write(data);
+        }).catch(onError);
+    } else {
+        client = new Net.Socket();
+
+        // set socket timeout
+        client.setTimeout(this.timeout);
+
+        client.connect(this.port, this.host, function () {
+            client.write(data);
+        });
+
+        client.on('data', onData);
+
+        client.on('timeout', function () {
+            error = new Error("socket timed out after " + self.timeout / 1000 + " seconds");
+            client.destroy();
+        });
+
+        client.on('error', onError);
+
+        client.on('close', onClose);
+    }
 }
 
 // takes items array and prepares payload for sending
@@ -127,10 +156,10 @@ function prepareData(items, with_timestamps, with_ns) {
         }
     }
 
-    var payload = new Buffer(JSON.stringify(data), 'utf8'),
-        header  = new Buffer(5 + 4); // ZBXD\1 + packed payload.length
+    var payload = Buffer.from(JSON.stringify(data), 'utf8'),
+        header = Buffer.alloc(5 + 4); // ZBXD\1 + packed payload.length
 
     header.write('ZBXD\x01');
     header.writeInt32LE(payload.length, 5);
-    return Buffer.concat([header, new Buffer('\x00\x00\x00\x00'), payload]);
+    return Buffer.concat([header, Buffer.from('\x00\x00\x00\x00'), payload]);
 }
